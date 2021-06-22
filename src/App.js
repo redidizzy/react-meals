f99c2abea5bf64f4705f83bfdd7b504805e7b836
Rediane Zemmouri <rediane.zemmouri@skaalab.com>
2021-06-22 12:18:16 +0100

    Add Meals component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from "./logo.svg"
 import "./App.css"
 import Header from "./components/Layout/Header"
+import Meals from "./components/Meals/Meals"
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   )
 }
